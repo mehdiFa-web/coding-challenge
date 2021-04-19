b0d972d15dc57ae2a11e8cf9d694531ac3d17fc6
El Mehdi El Fatihi <mehdielfatihi1111@gmail.com>
2021-04-19 10:22:11 -0400

    add delete route to remove categories

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/categories",[CategoryController::class,"index"])->name("categories.index");
 Route::post("/categories",[CategoryController::class,"store"])->name("categories.store");
+Route::delete("/categories/{id}",[CategoryController::class,"destroy"])->name("categories.destroy");
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
